6bb8772e42e42cf4dc749748b83cce497556f3f8
GlazHorsez <to5gogol@gmail.com>
2026-03-28 11:52:15 +0300

    added server url for front deploy

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // ВНИМАНИЕ: Проверь, что тут именно 4000 (как в сервере)
 const URL = import.meta.env.PROD
-    ? "https://onrender.com"
+    ? "https://kittychat-zh92.onrender.com"
     : "http://localhost:4000";
 
 export const socket = io(URL, {
